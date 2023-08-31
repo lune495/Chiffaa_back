91396a088d9646d35eec076a045f6c8166dcd98f
lune495 <badaralune9@gmail.com>
2023-08-31 01:24:48 +0000

    Update

--- app/Models/Outil.php
+++ app/Models/Outil.php
@@ -138,7 +138,7 @@ class Outil extends Model
         $name_env = self::getAPI();
         $critere = (is_numeric($id_critere)) ? "id:{$id_critere}" : $id_critere;
         $queryAttr = Outil::$queries[$queryName];
-        $response = $guzzleClient->get("{$name_env}/public/graphql?query={{$queryName}({$critere}){{$queryAttr}}}");
+        $response = $guzzleClient->get("{$name_env}graphql?query={{$queryName}({$critere}){{$queryAttr}}}");
         $data = json_decode($response->getBody(), true);
         return ($justone) ? $data['data'][$queryName][0] : $data;
     }
