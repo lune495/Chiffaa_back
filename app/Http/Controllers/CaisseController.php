5a8d52b8e2094ee632361bed480267004a4f4d16
Alioune badara dieng <badaralune9@gmail.com>
2023-09-07 01:51:10 +0100

    Updated

--- app/Http/Controllers/CaisseController.php
+++ app/Http/Controllers/CaisseController.php
@@ -245,7 +245,7 @@ class CaisseController extends Controller
             // if (!isset($errors))
             // {
                 // dd($results);
-                $pdf = PDF::loadView("pdf.situation-pdf",$results);
+                $pdf = PDF::loadView("pdf.situation-pdf2",$results);
                 return $pdf->stream();
             // }
        
