7ea15064fa849913e86254e658dad405d9e6cf53
Alioune badara dieng <badaralune9@gmail.com>
2023-10-09 19:15:14 +0100

    Update

--- app/GraphQL/Query/VenteQuery.php
+++ app/GraphQL/Query/VenteQuery.php
@@ -27,6 +27,7 @@ class VenteQuery extends Query
             'produit_id'          => ['type' => Type::int()],
             'created_at_start'    => ['type' => Type::string()],
             'created_at_end'      => ['type' => Type::string()],
+            'filtre_pharma'        => ['type' => Type::string()],
         ];
     }
 
@@ -41,6 +42,10 @@ class VenteQuery extends Query
         if (isset($args['reference']))
         {
             $query->where('numero',Outil::getOperateurLikeDB(),'%'.$args['reference'].'%');
+        }
+        if (isset($args['filtre_pharma']))
+        {
+            $query->where('statut',false);
         }
         if(isset($args['produit_id']))
         {
