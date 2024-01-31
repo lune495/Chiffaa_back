6c5ed68be9c2e1f0faf4d776895e5672e0347f4b
lune495 <badaralune9@gmail.com>
2024-01-31 11:49:52 +0000

    Update

--- app/Http/Controllers/CaisseController.php
+++ app/Http/Controllers/CaisseController.php
@@ -285,7 +285,7 @@ class CaisseController extends Controller
 
                     // PHARMACIE
                     $pharmacie = DB::table('ventes')
-                    ->select(DB::raw('SUM(montant)'))
+                    ->select(DB::raw('SUM(montant) AS montant'))
                     ->where('statut',false)
                     ->whereBetween('created_at', [$latestClosureDate ? $latestClosureDate->latest_date_fermeture : "0000-00-00 00:00:00", now()])
                     ->get();
