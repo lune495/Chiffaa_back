3cf69780fa359c8dff9f1c2b838043dd122571e1
lune495 <badaralune9@gmail.com>
2024-08-06 00:24:31 +0000

    Update

--- app/Http/Controllers/CaisseController.php
+++ app/Http/Controllers/CaisseController.php
@@ -219,7 +219,7 @@ class CaisseController extends Controller
         $results['nom_module'] = isset($module) ? $module->nom : "";
         $results['derniere_date_fermeture'] = $latestClosureDate->latest_date_fermeture;
         $results['current_date'] = now()->format('Y-m-d H:i:s');
-        // dd($results);
+        dd($results);
         $pdf = PDF::loadView("pdf.historique-pdf",$results);
         return $pdf->stream();
     }
