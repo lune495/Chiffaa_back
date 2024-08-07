43800df9a2701d8446248a9441602954a87b98a2
lune495 <badaralune9@gmail.com>
2024-08-07 13:28:28 +0000

    Update

--- app/Http/Controllers/CaisseController.php
+++ app/Http/Controllers/CaisseController.php
@@ -251,7 +251,7 @@ class CaisseController extends Controller
         // $pdf = PDF::loadView("pdf.historique-pdf",$results);
         // return $pdf->stream();
 
-        $services = Service::with(['user','medecin','module','element_services'])->where('module_id',$module_id)->first();
+        $service = Service::with(['user','medecin','module','element_services'])->where('module_id',$module_id)->first();
         $user = Auth::user();
         $data = [
             'service' => $service->id,
